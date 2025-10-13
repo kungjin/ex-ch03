edeabae81ac0904533f9d4bdde6757163ec23229
kungjin <vudchsdl123@gmail.com>
2025-10-13 16:45:40 +0900

    Init springboot Proj

---
+++ src/main/java/com/example/demo/controller/ExController.java
@@ -0,0 +1,57 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.service.ExService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class ExController {
+	
+	ExService exService;
+	public ExController(ExService exService) {
+		this.exService = exService;
+	}
+
+//	@GetMapping("/")
+//	public String getMethodName() {
+//		return "루트 경로";
+//	}
+	
+	@GetMapping("/")
+	public String getMethodName() {
+		return exService.sayHello("안녕");
+	}
+	
+	@GetMapping("/ex321")
+	public String getex321() {
+		return exService.ex321();
+	}
+	
+	@GetMapping("/ex322")
+	public String getex322() {
+		return exService.ex322();
+	}
+	
+	@GetMapping("/ex323")
+	public String getex323() {
+		return exService.ex323();
+	}
+	
+	@GetMapping("/ex324")
+	public String getex324() {
+		return exService.ex324();
+	}
+	
+	@GetMapping("/ex325")
+	public String getex325() {
+		return exService.ex325();
+	}
+	
+	@GetMapping("/ex326")
+	public String getex326() {
+		return exService.ex326();
+	}
+	
+}
---
+++ src/main/java/com/example/demo/service/ExService.java
@@ -0,0 +1,148 @@
+package com.example.demo.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ExService {
+	private static final Logger log = LoggerFactory.getLogger(ExService.class);
+	
+	public String sayHello(String hi) {
+		String msg = hi + "좋은랄";
+		System.out.println(msg);
+		log.info(msg);
+		log.debug(msg);
+		return hi;
+	}
+
+	public String ex321() {
+		int number = 1;
+		String result = "";
+		
+        if (number == 1) {
+           result = "if 블록입니다.";
+        } else if (number == 2) {
+           result = "else if 블록입니다.";
+        } else {
+        	result = "else 블록입니다.";
+        }
+        log.info(result);
+		return result;
+	}
+
+	public String ex322() {
+		int[] array = {1,2,3,4,5};
+		String result= "";
+		
+		for (int i = 0; i < array.length; i++) {
+			result += ("i = "+ array[i]+ "<br>");
+		}
+		log.info(result);
+		return result;
+	}
+		
+
+	public String ex323() {
+		int[] array = {1,2,3,4,5};
+		int i = 0;
+		String result = "";
+		
+		while (i< array.length) {
+			result += array[i] + "";
+			i++;
+		}
+		 log.info(result);
+		return result;
+	}
+	public interface Car{
+		void start();
+		void drive();
+		void stop();
+	}
+	public class Sonata implements Car{
+		public Sonata() {
+			log.info("== 출고 == Sonata");
+			}
+		@Override
+		public void start() {
+			log.info("Sonata 시동을 켭니다.");
+		}
+		@Override
+		public void drive() {
+			 log.info("Sonata 부드럽게 주행 중...");
+		}
+		@Override
+		 public void stop() {
+	            log.info("Sonata 정차합니다.");
+	        }
+	}
+	
+	
+	public class K5 implements Car{
+		public K5() {
+		log.info("== 출고 == K5");
+		}
+		@Override
+		public void start() {
+			log.info("K5 시동 ON!");
+		}
+		@Override
+		public void drive() {
+            log.info("K5 스포티하게 주행 중!");
+        }
+		@Override
+		 public void stop() {
+	            log.info("K5 브레이크 작동, 정차 완료.");
+	        }
+		
+	}
+	public String ex324() {
+	
+		Car car1 = new Sonata();
+		Car car2 = new K5();
+		
+			car1.start();
+	        car1.drive();
+	        car1.stop();
+
+	        car2.start();
+	        car2.drive();
+	        car2.stop();
+
+	        return "두 대의 차량을 출고하고 주행 테스트 완료.";
+	}
+
+	public String ex325() {
+		List<Integer> list = new ArrayList<>();
+		
+		
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		
+		Integer result = list.get(1);
+		return String.valueOf(result);
+	}
+
+	public String ex326() {
+		List<String> list = new ArrayList<>();
+		
+		list.add("public");
+		list.add("static");
+		list.add("void");
+		
+		for(int i = 0; i<list.size(); i++)
+		
+		list.remove(1);
+		int voidIndex = list.indexOf("void");
+			
+		return "void의 index = "+ voidIndex;
+	}
+
+
+	
+}
